57f77db232a33f01cd9a0d0f0d66af697a9f6f42
Amin <jjxamin@gmail.com>
2017-10-15 16:23:33 +0200

    A more modular approach!

--- assets/sketch.js
+++ assets/sketch.js
@@ -16,12 +16,53 @@ function preload() {
  
 //function setup() runs once, we include the canvas, buttons and an h2 element in it.
 function setup() {
+
+  //create the canvas element in P5
   createCanvas(600, 500);
-  h2 = createElement('h2', 'Le Quest Assignment by Amin Fatemi, Oct 2017');
-  h2.position(50, 5);
+
+  //function to create the necessary elements, title, buttons, etc.
+  createCnvElements();
+
+  //functions to play the music and create the play/pause buttons and volume slider control 
+  musicPlay();
+  musicControl();
   
-  //play the song and its related controls
+  //function to manage behavior of the next and previous buttons
+  buttonBehavior();  
+}
+
+//In P5 function draw() loops over and over again
+function draw(){
+  background(220);    //sets background color, a bright gray
+  
+  //presentation layer of the app, display the images, text, clickable div and the popup box
+  display();
+
+  // set volume of the song
+  musicVolSet();
+
+}
+
+//function to manage behavior of the next and previous buttons
+function buttonBehavior(){
+  buttonNext.mousePressed(changeSlideBw);
+  buttonNext.mousePressed(playSongBw);
+  buttonPrev.mousePressed(changeSlideFw);
+  buttonPrev.mousePressed(playSongFw);
+}
+
+//play the song 
+function musicPlay(){
   song[2].loop();
+}
+
+//volume slider callback to set volume of the music
+function musicVolSet(){
+  song[2].setVolume(volslider.value());
+}
+
+//create the control buttons for the song and volume
+function musicControl(){
   song[2].setVolume(0.3);
   playButton = createButton('Pause');
   playButton.addClass('songcontrolbuttons');
@@ -30,51 +71,73 @@ function setup() {
   playButton.mousePressed(togglePlaySong);
   stopButton.mousePressed(stopSong);
   volslider = createSlider(0,1,0.5,0.01);
-
-  //buttons for the image slider next and previous, the style and event listeners
-  button1 = createButton('< Previous');
-  button1.addClass('buttons');
-  button2 = createButton('Next >');
-  button2.addClass('buttons');  
-  button1.position(width-500, height-70);
-  button2.position(width-175, height-70);
-  button1.mousePressed(changeSlideBw);
-  button2.mousePressed(changeSlideFw);
 }
 
-  //In P5 function draw() loops over and over again
-function draw(){
-  background(220);    //sets background color, a bright white here
-  imageMode(CENTER);  // Modifies the coordinates from which images are drawn 
-  image(img[index], width/2,height/2,width*0.8,height*0.6);
+function createCnvElements(){
+  h2 = createElement('h2', 'Le Quest Assignment by Amin Fatemi, Oct 2017');
+  h2.position(48, 5);
 
-  //set volume of the song
-  song[2].setVolume(volslider.value());
+  //buttons for the image slider next and previous, the style and event listeners
+  buttonNext = createButton('< Previous');
+  buttonNext.addClass('buttons');
+  buttonPrev = createButton('Next >');
+  buttonPrev.addClass('buttons');  
+  buttonNext.position(width-500, height-70);
+  buttonPrev.position(width-175, height-70);
+}
 
-  //display text from the imageText arraye
-  textSize(25);
-  textAlign(CENTER);
-  fill(12,100,230);
-  noStroke();
-  text(imageText[index], width/2 + random(-2,2), height*0.85);
+function display(){
+  displayImages();
+  displayText();  
+  displayClickable();
+  displayPopup();
+}
 
+function displayClickable(){
   //get the element with id #par and save to the global variable clickable
-  clickable = select('#par');
-  clickable.position(width/5,height/2);  
+  clickable = select('#clickable');
+  clickable.position(width/5,height/2);
+
+  //utilize js closures to change style with mouse over and mouse out events
   var Highlight = changeStyle('37FF00','B85DAC','0.8');
   var Unhighlight = changeStyle('00AAFF','DF7B77','0.2');
   clickable.mouseOver(Highlight); //hover over and style changes
   clickable.mouseOut( Unhighlight); //hover out and style reverts back
-  clickable.mousePressed(popup);  //upon mouseclick a popup box appears
+}
+
+//function closure to highlight and unhighlight the clickable div upon hover in and out
+function changeStyle(borderColor, backgroundColor, opacity) {
+  return function() {
+  this.style('border', '2px solid #' + borderColor);
+  this.style('background-color', '#' + backgroundColor);
+  this.style('opacity', opacity);
+  };
+}
+
+function displayPopup(){
+  clickable.mousePressed(popup);  //upon mouseclick a popup box appears  
+}
+
+function displayImages(){
+  imageMode(CENTER);  // Modifies the coordinates from which images are drawn 
+  image(img[index], width/2,height/2,width*0.8,height*0.6);
+}
+
+function displayText(){ 
+  textSize(25);
+  textAlign(CENTER);
+  fill(12,100,230);
+  noStroke();
+  text(imageText[index], width/2 , height*0.85);
 }
 
 //function to show a popup box when the clickable div is clicked
 function popup(){
   popbox = select('#popup'); //define the local variable
   popbox.position(width/5+90,height/2.5);
-  popbox.mousePressed(popupHide); //upon mouse click on popup box it disappears
-  button1.mousePressed(popupHide);
-  button2.mousePressed(popupHide);
+  popbox.mousePressed(popupHide); //upon mouse click (on popup box itself) it disappears
+  buttonNext.mousePressed(popupHide);
+  buttonPrev.mousePressed(popupHide);
   popbox.style('display:inline');
 }
 
@@ -83,14 +146,7 @@ function popupHide(){
   popbox.style('display:none');
 }
 
-//function closure to highlight and unhighlight the clickable div upon hover in and out
-function changeStyle(borderColor, backgroundColor, opacity) {
-  return function() {
-  clickable.style('border', '2px solid #' + borderColor);
-  clickable.style('background-color', '#' + backgroundColor);
-  clickable.style('opacity', opacity);
-  };
-}
+
 
 //function to show previou image and play a sound
 function changeSlideBw() {
@@ -99,16 +155,23 @@ function changeSlideBw() {
   } else{
     index = index - 1;
   }
-  song[1].play();
 }
 
-//function to show next image and play a sound
+//function to show next image upon click
 function changeSlideFw(){
   if (index == img.length-1){
     index =  0;
   } else{
     index = index + 1;
-  }
+  } 
+}
+
+//function to play a sound upon click
+function playSongBw(){
+  song[1].play();
+}
+
+function playSongFw(){
   song[0].play();
 }
 
